5c7b5ad4306491c52436ec6b465f74071cfe40a8
Balram kumar <balramraj887765@gmail.com>
2026-05-20 22:00:32 +0530

    Time: 14 ms (48.85%), Space: 41.9 MB (70.48%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,13 +3,13 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int low=1;
-        int high=n;
+        int low=0;
+        int high=n-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(isBadVersion(mid)==false) low=mid+1;
-            else high=mid-1;
+            if(isBadVersion(mid)==true) high=mid-1;
+            else low=mid+1;
         }
-        return low; // ans where the first bad version
+        return low;
     }
 }
