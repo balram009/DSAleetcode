3792d81186f7e8a5d888aefb90258457b8dada92
Balram kumar <balramraj887765@gmail.com>
2026-05-21 21:50:05 +0530

    Time: 0 ms (100%), Space: 41.8 MB (86.06%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -3,11 +3,11 @@ class Solution {
         long low=1;
         long high=num;
         while(low<=high){
-            long mid=low+(high-low)/2;
+            long mid=(low+high)/2;
             long ans=mid*mid;
             if(ans==(long)num) return true;
-            else if(ans<(long)num) low=mid+1;
-            else high=mid-1;
+            else if(ans>(long)num) high=mid-1;
+            else low=mid+1;
         }
         return false;
     }
