93def92f517f8a22355b4f205854dff6a49ceee8
Balram kumar <balramraj887765@gmail.com>
2026-06-10 21:16:34 +0530

    Time: 0 ms (100%), Space: 42.6 MB (21.85%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -5,11 +5,11 @@ class Solution {
         else{
             int sum=0;
             while(n!=0){
-            int rem=n%10;
-            sum=sum+rem*rem;
-            n=n/10;
-        }
-        return isHappy(sum);
+                int rem=n%10;
+                sum=sum+rem*rem;
+                n=n/10;
+            }
+            return isHappy(sum);
         }
     }
 }
